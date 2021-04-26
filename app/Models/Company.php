c27b92542e55e9931f9aa2f3f7c17f51fbd705d7
Dmitry <kashko.dmitry@gmail.com>
2021-04-26 22:31:06 +0300

    create company migration && seed && model

---
+++ app/Models/Company.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'list_bonus',
+        'small_descriptions',
+        'video',
+        'img',
+        'amount',
+        'user_id',
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $casts = [
+        'list_bonus' => 'array'
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $attributes = [
+        'list_bonus' => '{}'
+    ];
+}
---
+++ database/migrations/2021_04_26_185617_create_companies_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('list_bonus');
+            $table->text('small_descriptions');
+            $table->string('video');
+            $table->string('img');
+            $table->float('amount');
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
